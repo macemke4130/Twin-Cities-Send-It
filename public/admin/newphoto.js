e312b66ffd8cc823aabc5ef05d6bfa3c7e304585
Lucas Mace <lucasmace4130@gmail.com>
2021-07-27 23:28:42 -0500

    List of hills populating on photo upload page

--- public/admin/newphoto.js
+++ public/admin/newphoto.js
@@ -16,25 +16,51 @@ const gql = async (ask) => {
     return r.data;
 }
 
-const upload = async (e) => { 
-    e.preventDefault();
-    const newPhoto = e.srcElement.childNodes[1].files[0];
-    const hillId = document.getElementById("hillId").value;
+const getAllHills = async () => {
+    // Grabs list of all hills and populates select list --
+    const r = await gql(`{allHills { id, name }}`);
+    const allHills = r.allHills;
 
+    const select = document.getElementById("hillId");
+
+    for (let i = 0; i < allHills.length; i++) {
+        let hill = document.createElement("option");
+        hill.value = allHills[i].id;
+        hill.innerHTML = allHills[i].name;
+        select.appendChild(hill);
+    }
+}
+
+const upload = async (e) => { 
+    // Prevents form submission refresh --
+    e.preventDefault();
+
+    // Selected file and assigned hill --
+    const newPhoto = e.srcElement.childNodes[1].files[0];
+    const hillId = Number(document.getElementById("hillId").value);
+
+    // Preparing the photo for upload --
     const data = new FormData();
     data.append("img", newPhoto);
 
+    // Upload API endpoint push --
     const req = await fetch("/upload", {
         method: "POST",
         headers: { },
         body: data
     });
 
+    // Response from server with file info --
     const file = await req.json();
 
     const r = await gql(`mutation { newPhoto(hill_id: ${hillId}, filename: "${file.filename}") { insertId } }`);
-    console.log(r.newPhoto.insertId);
+    if (r) {
+        console.log(r.newPhoto.insertId);
+        alert("New photo uploaded to " + file.path);
+        // window.location.href = "./panel.html";
+    }
 }
 
 const form = document.getElementById('photoUpload');
 form.addEventListener('submit', upload);
+getAllHills();
