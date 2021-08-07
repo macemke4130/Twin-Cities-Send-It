d92df9f658ccb85e643547e4bdb31b1d602458b7
Lucas Mace <lucasmace4130@gmail.com>
2021-08-06 23:50:02 -0500

    Live View button added, no logic yet

--- public/admin/panel.js
+++ public/admin/panel.js
@@ -18,9 +18,12 @@ const getAllHills = async () => {
     for (let i = 0; i < allHills.length; i++) {
         // Create Container Div --
         let hillDiv = document.createElement('div');
-        hillDiv.className = "admin-hill-div";
 
-        if (i % 2 === 0) hillDiv.className = "admin-hill-div grey-bg";
+        if (i % 2 === 0) { 
+            hillDiv.className = "admin-hill-div grey-bg";
+        } else {
+            hillDiv.className = "admin-hill-div";
+        }
 
         // Create Link --
         let newLink = document.createElement('a');
@@ -33,20 +36,26 @@ const getAllHills = async () => {
         newSpan.className = "edit-hill";
         newSpan.appendChild(newText);
 
+        // Create Controls Div --
+        let controlDiv = document.createElement('div');
+        controlDiv.className = "controls";
+
         // Create Edit Button --
-        let newBtn = document.createElement('button');
-        newBtn.innerText = "Edit";
+        let editBtn = document.createElement('button');
+        editBtn.innerText = "Edit";
 
         // Create View Live Button --
+        let liveBtn = document.createElement('button');
+        liveBtn.innerText = "View Live";
 
         // Modify DOM --
         hillDiv.appendChild(newLink);
-        hillDiv.appendChild(newBtn);
+        hillDiv.appendChild(controlDiv);
+        controlDiv.appendChild(editBtn);
+        controlDiv.appendChild(liveBtn)
         newLink.appendChild(newSpan);
-        
         div.appendChild(hillDiv);
     }
 }
 
-// Run --
 getAllHills();
